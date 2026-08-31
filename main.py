881bf838e39d75150c8e9d2ad412a19c320c7481
Brian107329 <brian107329@mcpsva.org>
2026-08-31 10:54:31 -0400

    Hello

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print("Hello World" == "Today is a great day")
+"""
 
-print("Today i'll be doing an activity")
+"""
 
+print("Today administration ask me a couple of question: ")
+q1 = input("Question one is how has school been treating you? ")
+q2 = input('Question two is are you interested in some new courses? ')
+q3 = input('Question three is how are your teachers been? ')
+q4 = input('Question four is do you have any complaints about this school? ')
+q5 = input('Question five is what do you find the most interest in this school? ')
+print(" q1 = " + q1)
