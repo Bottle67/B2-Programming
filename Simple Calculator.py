3a8cdbb6397824396deba9b4f0f971685253210d
Brian107329 <brian107329@mcpsva.org>
2026-09-22 10:26:03 -0400

    Hello

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,11 +1,14 @@
 """
-Filename: madlibs.py
+Filename: simple calcuator.py
 Author: <Trejo Young, Brian>
-Created: <09/15/2026>
+Created: <09/22/2026>
 Instructor: Burgess
 """
 
 print("This will be a simple calculation from coding")
-q1 = input("Put a number")
-q2 = input("put in another number")
-
+q1 = int(input("Put in a number"))
+q2 = int(input("put in another number"))
+print(f"{q1} + {q2} = {q1 + q2}")
+print(f"{q1} - {q2} = {q1 - q2}")
+print(f"{q1} * {q2} = {q1 * q2}")
+print(f"{q1} / {q2} = {q1 / q2}")
